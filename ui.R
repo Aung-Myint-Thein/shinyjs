aae46221c33b5d5fa9577de5ea1c68c24934aeeb
Aung Myint Thein <myintthein.aung@insead.edu>
2013-07-02 14:47:20 +0800

    testing js

--- ui.R
+++ ui.R
@@ -26,7 +26,22 @@ shinyUI(pageWithSidebar(
                 value = 500)
 	,
 	
-	incrementButton("thisthis")
+	incrementButton("thisthis"),
+	HTML('
+	<div class="modal hide fade">
+		<div class="modal-header">
+			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+			<h3>Modal header</h3>
+		</div>
+		<div class="modal-body">
+		<p>One fine body…</p>
+		</div>
+		<div class="modal-footer">
+			<a href="#" class="btn">Close</a>
+			<a href="#" class="btn btn-primary">Save changes</a>
+		</div>
+	</div>
+	')
   ),
 
   # Show a plot of the generated distribution
